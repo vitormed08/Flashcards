01da062e3a32dd0163cbdc902bd465a6f8038865
vitu loco <166011347+vitormed08@users.noreply.github.com>
2024-11-28 08:00:42 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma Função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma Variável?',
+    'Variável é um espaço na memória do computador, criada para armazenar informações.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é C#?',
+    'O C# é uma linguagem de programação.'
+)
